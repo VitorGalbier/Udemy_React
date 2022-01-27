7723ccff1a5bfdf03c6b065e28d1323ce38b0cd0
Vitor Galbier <vitor.galbier@gmaail.com>
2022-01-27 16:30:40 -0300

    learning about conditional and function in React

--- Indecision_app/public/scripts/app.js
+++ Indecision_app/public/scripts/app.js
@@ -4,11 +4,79 @@ console.log("App.js is running");
 // JSX - Javascript XML
 // let template = <p> This is JSX </p>; isso é jsx
 // let template = /*#__PURE__*/React.createElement("p", null, " This is JSX ");
+
+var appObject = {
+    title: "Indecision App!",
+    subTitle: "Put your life in the hands of a computer"
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        appObject.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        appObject.subTitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item 1"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "item 2"
+        )
+    )
 );
+
+var usuario = {
+    name: "Vitor",
+    age: 27,
+    location: "Campinas"
+
+    // let name = 'Mike'
+    // let age = 20
+    // let location = 'Santos'
+    // undefined é útil pq não vai mostrar nada
+
+};function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        usuario.name ? usuario.name : 'Anonymous'
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        usuario.age
+    ),
+    getLocation(usuario.location)
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot); /*Dois argumentos: o primeiro o JSX que queremos renderizar, o segundo é o dom onde queremos */
+ReactDOM.render(template2, appRoot); /*Dois argumentos: o primeiro o JSX que queremos renderizar, o segundo é o dom onde queremos */
--- Indecision_app/public/src/app.js
+++ Indecision_app/public/src/app.js
@@ -2,7 +2,47 @@ console.log("App.js is running")
 // JSX - Javascript XML
 // let template = <p> This is JSX </p>; isso é jsx
 // let template = /*#__PURE__*/React.createElement("p", null, " This is JSX ");
-let template = <h1>Indecision App!</h1>
+
+let appObject = {
+    title: "Indecision App!",
+    subTitle: "Put your life in the hands of a computer"
+}
+
+let template = (
+    <div>
+        <h1>{appObject.title}</h1> 
+        <p>{appObject.subTitle}</p>
+        <ol>
+            <li>Item 1</li>
+            <li>item 2</li>
+        </ol>
+    </div>);
+
+let usuario = {
+    name: "Vitor",
+    age: 27,
+    location: "Campinas"
+}
+
+// let name = 'Mike'
+// let age = 20
+// let location = 'Santos'
+// undefined é útil pq não vai mostrar nada
+
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>;
+    }
+}
+
+let template2 = (
+    <div>
+        <h1>{usuario.name ? usuario.name : 'Anonymous'}</h1>
+        <p>Age: {usuario.age}</p>
+        {getLocation(usuario.location)}
+    </div>
+)
+
 let appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot); /*Dois argumentos: o primeiro o JSX que queremos renderizar, o segundo é o dom onde queremos */
+ReactDOM.render(template2, appRoot); /*Dois argumentos: o primeiro o JSX que queremos renderizar, o segundo é o dom onde queremos */
